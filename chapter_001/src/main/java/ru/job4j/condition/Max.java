53dbe91e1cae089408ca82917629e2dd0e5aacaf
Aleksandr Karpachov <xalexandrx9@gmail.com>
2019-04-10 17:16:46 +0300

    4.2. Максимум из двух чисел[#130793]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Max {
+    /**
+     * Method for determine maximum of two digit
+     * @param left value
+     * @param right value
+     * @return int max
+     */
+    public int max(int left, int right) {
+        return left > right ? left : right;
+    }
+}
