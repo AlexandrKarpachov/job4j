bcb5aa2c80bb79c97431bb9b458f4243bd35910a
Aleksandr Karpachov <xalexandrx9@gmail.com>
2019-04-10 10:22:05 +0300

    "3.1. Элементарный калькулятор.[#130823]"

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculator.java
@@ -0,0 +1,52 @@
+package ru.job4j.calculate;
+/**
+ *Calculator
+ * support only: add, subtract, div and multiple.
+ * @version 0.01
+ * @author Aleksandr Karpachov
+ */
+
+public class Calculator {
+    /**
+     * Method adds two values
+     * @param first first value
+     * @param second second value
+     * @return result
+     */
+
+    public double add(double first, double second){
+        return first + second;
+    }
+
+    /**
+     * Method subtracts two values
+     * @param first value
+     * @param second value
+     * @return result
+     */
+    public double subtract(double first, double second){
+        return first - second;
+    }
+
+    /**
+     * Method multiplies two values
+     * @param first value
+     * @param second value
+     * @return result
+     */
+    public double multiply (double first, double second){
+        return first * second;
+    }
+
+    /**
+     * Method divides two values
+     * @param first first
+     * @param second second
+     * @return result. If first and second value are equal 0, return NaN,
+     * else if first is positive and second == 0, return Infinity
+     * else if first is negative and second == 0, return -Infinity
+     */
+    public double divide (double first, double second){
+        return first / second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculatorTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.calculate;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class CalculatorTest {
+    @Test
+    public void whenAddOnePlusOneThenTwo()  {
+        Calculator calc = new Calculator();
+        double result = calc.add(1D, 1D);
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSubtractTwoMinusOneThenOne(){
+        Calculator calc = new Calculator();
+        double result = calc.subtract(2D, 1D);
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenMultiplyTwoOnTwoThenFour(){
+        Calculator calc = new Calculator();
+        double result = calc.multiply(2D, 2D);
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenDivide5On2Then2AndHalf(){
+        Calculator calc = new Calculator();
+        double result = calc.divide(5D, 2D);
+        double expected = 2.5;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenDivide0onPositiveThenInfinity() {
+        Calculator calc = new Calculator();
+        double result = calc.divide(5D, 0);
+        boolean isInfinity = Double.isInfinite(result);
+        assertThat(isInfinity, is(true));
+    }
+
+    @Test
+    public void whenDivide0on0ThenNaN() {
+        Calculator calc = new Calculator();
+        double result = calc.divide(0, 0);
+        boolean resultIsNaN = Double.isNaN(result);
+        assertThat(resultIsNaN, is(true));
+    }
+
+
+}
