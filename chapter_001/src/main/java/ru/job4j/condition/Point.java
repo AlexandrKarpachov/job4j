9c4f89d453c193bce24ed926b2243395c028e7a1
Aleksandr Karpachov <xalexandrx9@gmail.com>
2019-04-17 16:24:18 +0300

    1. Рефакторинг - Расстояние между точками.[#130928]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,21 +1,32 @@
 package ru.job4j.condition;
 
 /**
- * Class for make solves with points
+ * Points class
  */
 public class Point {
+    private int x;
+    private int y;
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
     /**
-     * Method for solve distance between two points by coordinates (x, y)
-     * @param x1 coordinate x of first point,
-     * @param y1 coordinate y of first point,
-     * @param x2 coordinate x of second point,
-     * @param y2 coordinate y of second point,
+        * Method for solve distance between two points by coordinates (x, y)
+     * @param that another Point
      * @return double
      */
 
-    public double distance(int x1, int y1, int x2, int y2) {
-        double first = Math.pow(x2 - x1, 2);
-        double second = Math.pow(y1 - y2, 2);
+    public double distance(Point that) {
+        double first = Math.pow(this.x - that.x, 2);
+        double second = Math.pow(this.y - that.y, 2);
         return Math.sqrt(first + second);
     }
+
+    /**
+     * this method prints x and y of this Point
+     */
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
 }
+
