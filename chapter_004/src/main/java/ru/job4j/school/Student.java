e1fb521fc068e948cfb580967d8664a80f72038e
Aleksandr Karpachov <xalexandrx9@gmail.com>
2019-05-23 14:04:03 +0300

    Stream Api Improvement [#130760]

--- chapter_004/src/main/java/ru/job4j/school/Student.java
+++ chapter_004/src/main/java/ru/job4j/school/Student.java
@@ -1,12 +1,14 @@
 package ru.job4j.school;
 
+import java.util.Comparator;
 import java.util.Objects;
+
 /**
  * @author Aleksand Karpachov
  * @since 19.05.2019
  * Class for storing Student info
  */
-public class Student {
+public class Student implements Comparable<Student> {
     private int score;
     private String name;
     private String surname;
@@ -50,5 +52,13 @@ public class Student {
     public String toString() {
         return String.format("Student{name = %s, surname = %s, score = %d",
                 name, surname, score);
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Comparator.comparing(Student::getScore, Comparator.reverseOrder())
+                .thenComparing(Student::getSurname)
+                .thenComparing(Student::getName)
+                .compare(this, o);
     }
 }
