5da9f1cbdf98ac3bca73db1d038d590584c54d32
Aleksandr Karpachov <xalexandrx9@gmail.com>
2019-05-30 17:32:28 +0300

    Even numbers iterator [#145911]

---
+++ chapter_005/src/main/java/ru/job4j/iterators/EvenNumbersIterator.java
@@ -0,0 +1,45 @@
+package ru.job4j.iterators;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * {@code Even Numbers Iterator} returns the next even element of the array.
+ *
+ * @author Alexander Karpachov
+ * @since 30.05.2019
+ */
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private final int[] array;
+    private int count = -1;
+
+    public EvenNumbersIterator(int[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return getNextEven() >= 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        count = getNextEven();
+        return array[count];
+    }
+
+    private int getNextEven() {
+        int result = -1;
+        for (int i = count + 1; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
