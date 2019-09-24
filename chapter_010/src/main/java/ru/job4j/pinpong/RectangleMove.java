b34d99fff38b71fea3eb848e682079351cd9ee4f
Aleksandr Karpachov <xalexandrx9@gmail.com>
2019-09-24 16:54:10 +0300

    ping pong

---
+++ chapter_010/src/main/java/ru/job4j/pinpong/RectangleMove.java
@@ -0,0 +1,33 @@
+package ru.job4j.pinpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+	private final Rectangle rect;
+	private final int fieldWidth;
+
+	public RectangleMove(Rectangle rect, int fieldWidth) {
+		this.rect = rect;
+		this.fieldWidth = fieldWidth;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			int direction = 1;
+			if (this.rect.getX() == this.fieldWidth - rect.getWidth()) {
+				direction = -1;
+			}
+			if (this.rect.getX() == 0) {
+				direction = 1;
+			}
+			this.rect.setX(this.rect.getX() + direction);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+
+		}
+	}
+}
